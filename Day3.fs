e0811cbc92944f23a58b25de375fd99c836b8430
Markus Reynolds <markusreynolds1989@gmail.com>
2022-12-11 12:05:11 -0600

    Star 1

---
+++ Day3.fs
@@ -0,0 +1,63 @@
+﻿module Day3
+
+open System
+open System.Text
+
+let path = "C:\Users\marku\Code\F#\Advent2022\Data\day_3.txt"
+let test =
+    "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"
+
+let lowerLetters = [ 'a' .. 'z' ]
+let upperLetters = [ 'A' .. 'Z' ]
+let letters = lowerLetters @ upperLetters
+
+let lettersTable =
+    List.fold
+        (fun acc value ->
+            let currentIndex = fst acc
+            let nextIndex = fst acc + 1
+            let listState = snd acc
+            (nextIndex, List.append [ (value, currentIndex) ] listState))
+        (1, [])
+        letters
+    |> snd
+    |> Map.ofList
+
+let parseRucksackContents (lines: string []) : seq<string * string> =
+    seq {
+        for line in lines do
+            let line = line.Replace("\r", "")
+            let half = line.Length / 2
+            let length = line.Length
+            (line.Substring(0, half), line.Substring(half, length - half))
+    }
+
+let parseRucksackContentsToPriorities (rucksacks: seq<string * string>) : seq<seq<int> * seq<int>> =
+    rucksacks
+    |> Seq.map (fun rucksack ->
+        ((fst rucksack)
+         |> Seq.map (fun value -> Map.find value lettersTable),
+         (snd rucksack)
+         |> Seq.map (fun value -> Map.find value lettersTable)))
+
+let calculateCommonPriorities (rucksacks: seq<seq<int> * seq<int>>) =
+    rucksacks
+    |> Seq.map (fun rucksack ->
+        let firstRuck, secondRuck = rucksack
+        let firstRuck = firstRuck |> Set.ofSeq
+        let secondRuck = secondRuck |> Set.ofSeq
+
+        Set.intersect firstRuck secondRuck
+        |> Set.toArray
+        |> (fun value -> value[0]))
+
+let calculatePrioritiesOfUnions lines =
+    parseRucksackContents lines
+    |> parseRucksackContentsToPriorities
+    |> calculateCommonPriorities
+    |> Seq.sum
--- Program.fs
+++ Program.fs
@@ -1,10 +1,10 @@
 ﻿module Program
-
 open System.IO
 
 [<EntryPoint>]
 let main args =
 
-    let lines = File.ReadAllLines("")
-
+    let lines = File.ReadAllLines(Day3.path)
+    //let lines = Day3.test.Split("\n")
+    printfn $"%A{Day3.calculatePrioritiesOfUnions lines}"
     0
