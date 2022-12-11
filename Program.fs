035311f8e473db71be6cb2fb3c22f48142155af8
Markus Reynolds <markusreynolds1989@gmail.com>
2022-12-10 21:01:03 -0600

    Star 1 and 2 day 1

---
+++ Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+open System.IO
+
+let rec createElfSupplies (lines: list<string>) (acc: list<int>) : list<list<int>> =
+    match lines with
+    | [] -> []
+    | value :: _ when String.IsNullOrWhiteSpace(value) -> (createElfSupplies (List.tail lines) List.empty) @ [ acc ]
+    | _ -> createElfSupplies (List.tail lines) (acc @ [ int (List.head lines) ])
+
+let elfSuppliesMax lines =
+    
+    createElfSupplies lines []
+    |> List.map List.sum
+    |> List.max
+
+let elfSuppliesTopThree lines =
+    
+    createElfSupplies lines []
+    |> List.map List.sum
+    |> List.toArray
+    |> Array.sortDescending
+    |> Array.take 3
+    |> Array.sum
+
+[<EntryPoint>]
+let main args =
+    let lines =
+        File.ReadAllText("C:\Users\marku\Code\F#\Advent2022\elf_calories.txt")
+        |> (fun lines -> Seq.append lines "\n" |> String.Concat)
+        |> (fun lines -> lines.Split("\n"))
+        |> Seq.toList
+        
+    printfn $"{elfSuppliesTopThree lines}"
+    0
