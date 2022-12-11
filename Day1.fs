01cc9d20888fcc754f8c993b867f57eafc60fc33
Markus Reynolds <markusreynolds1989@gmail.com>
2022-12-10 21:57:51 -0600

    Modularize

---
+++ Day1.fs
@@ -0,0 +1,23 @@
+﻿module Day1
+
+open System
+
+let createElfSupplies (lines: string []) =
+    seq {
+        let tempCalories = ResizeArray<int>()
+
+        for line in lines do
+            if String.IsNullOrWhiteSpace(line) then
+                yield Seq.sum tempCalories
+                tempCalories.Clear()
+            else
+                tempCalories.Add(int line)
+    }
+
+let elfSuppliesMax lines = createElfSupplies lines |> Seq.max
+
+let elfSuppliesTopThree lines =
+    createElfSupplies lines
+    |> Seq.sortDescending
+    |> Seq.take 3
+    |> Seq.sum
--- Program.fs
+++ Program.fs
@@ -1,30 +1,11 @@
-﻿open System
+﻿module Program
+
 open System.IO
-
-let createElfSupplies (lines: string []) =
-    seq {
-        let tempCalories = ResizeArray<int>()
-
-        for line in lines do
-            if String.IsNullOrWhiteSpace(line) then
-                yield Seq.sum tempCalories
-                tempCalories.Clear()
-            else
-                tempCalories.Add(int line)
-    }
-
-let elfSuppliesMax lines = createElfSupplies lines |> Seq.max
-
-let elfSuppliesTopThree lines =
-    createElfSupplies lines
-    |> Seq.sortDescending
-    |> Seq.take 3
-    |> Seq.sum
 
 [<EntryPoint>]
 let main args =
     let lines =
         File.ReadAllLines("C:\Users\marku\Code\F#\Advent2022\elf_calories.txt")
 
-    printfn $"{elfSuppliesTopThree lines}"
+    printfn $"{Day1.elfSuppliesTopThree lines}"
     0
