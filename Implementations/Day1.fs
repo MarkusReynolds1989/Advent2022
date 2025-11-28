3cada438b6fb50d413a1f7d74c6eb7008a35cab0
Markus Reynolds <markusreynolds1989@gmail.com>
2025-11-28 17:44:11 -0600

    chore: Cleanup SOLID

--- Implementations/Day1.fs
+++ Implementations/Day1.fs
@@ -2,22 +2,82 @@
 module Day1
 
 open System
+open Microsoft.Extensions.Logging
 
-let filePath = "Data/elf_calories.txt"
+let filePath = "elf_calories.txt"
 
-let createElfSupplies (lines: string[]) =
+type ListLine =
+    | End
+    | Line of int
+
+type StarSwitch =
+    | StarOne
+    | StarTwo
+
+let parseLine (logger: ILogger) (line: string) : Result<ListLine, string> =
+    let mutable output = ref 0
+
+    if String.IsNullOrWhiteSpace(line) then
+        Ok End
+    else
+        Int32.TryParse(line, output)
+        |> function
+            | true -> Ok(Line output.Value)
+            | false ->
+                let failureError = $"Failed to parse {line}"
+                logger.LogError(failureError)
+                Error failureError
+
+let parseElfSuppliesInput (logger: ILogger) (lines: string seq) : Result<ListLine list, string> =
+    logger.LogInformation("Parsing elf supplies list")
+
+    lines
+    |> Seq.fold
+        (fun accResult line ->
+            match accResult with
+            | Error err -> Error err
+            | Ok accumulatedList ->
+                match parseLine logger line with
+                | Ok parsedValue -> Ok(parsedValue :: accumulatedList)
+                | Error err -> Error err)
+        (Ok [])
+
+let createElfSupplies (logger: ILogger) (lines: ListLine seq) : int seq =
+    logger.LogInformation("Creating elf supplies")
+    let mutable accumulator = 0
+
     seq {
-        let tempCalories = ResizeArray<int>()
-
         for line in lines do
-            if String.IsNullOrWhiteSpace(line) then
-                yield Seq.sum tempCalories
-                tempCalories.Clear()
-            else
-                tempCalories.Add(int line)
+            match line with
+            | End ->
+                yield accumulator
+                accumulator <- 0
+            | Line number -> accumulator <- accumulator + number
     }
 
-let elfSuppliesMax lines = createElfSupplies lines |> Seq.max
+let calculateElfSupplies (logger: ILogger) (starSwitch: StarSwitch) (lines: string seq) : Result<int, string> =
+    let elfSupplies =
+        match parseElfSuppliesInput logger lines with
+        | Ok parsedSupplies -> Ok(createElfSupplies logger parsedSupplies)
+        | Error error -> Error error
 
-let elfSuppliesTopThree lines =
-    createElfSupplies lines |> Seq.sortDescending |> Seq.take 3 |> Seq.sum
+    match starSwitch with
+    | StarOne ->
+        logger.LogInformation("Getting the maximum elf supplies")
+
+        match elfSupplies with
+        | Ok result ->
+            let final = result |> Seq.max
+            logger.LogInformation("Maximum is: {Final}", final)
+            Ok final
+        | Error error -> Error error
+
+    | StarTwo ->
+        logger.LogInformation("Getting the top three elf supplies")
+
+        match elfSupplies with
+        | Ok result ->
+            let final = result |> Seq.sortDescending |> Seq.take 3 |> Seq.sum
+            logger.LogInformation("Sum of top three is: {Result}", result)
+            Ok final
+        | Error error -> Error error
--- Testing/Tests.fs
+++ Testing/Tests.fs
@@ -1,16 +1,44 @@
 ﻿module Tests
 
-open Xunit
 open System.IO
 
-[<Fact>]
-let ``Day 1 Star 1`` () =
-    let lines = File.ReadAllLines Day1.filePath
-    let result = Day1.elfSuppliesMax lines
-    Assert.Equal(69_883, result)
+open Xunit
 
-[<Fact>]
-let ``Day 1 Star 2`` () =
-    let lines = File.ReadAllLines Day1.filePath
-    let result = Day1.elfSuppliesTopThree lines
-    Assert.Equal(207_576, result)
+open Microsoft.Extensions.Logging
+
+type DayOneFixture() =
+    let lines = File.ReadAllLines Day1.filePath |> Array.toSeq
+
+    let loggerFactory =
+        LoggerFactory.Create(fun builder ->
+            builder.AddConsole() |> ignore
+            builder.AddDebug() |> ignore
+            builder.SetMinimumLevel(LogLevel.Debug) |> ignore)
+
+    let logger = loggerFactory.CreateLogger<DayOneFixture>()
+
+    member this.Logger = logger
+    member this.Lines = lines
+
+    interface System.IDisposable with
+        member this.Dispose() = loggerFactory.Dispose()
+
+
+[<Collection("Day1Tests")>]
+type Day1Tests(fixture: DayOneFixture) =
+    let data = fixture.Lines
+    let logger = fixture.Logger
+
+    interface IClassFixture<DayOneFixture>
+
+    [<Fact>]
+    member this.``Day 1 Star 1``() =
+        match Day1.calculateElfSupplies logger Day1.StarSwitch.StarOne data with
+        | Ok result -> Assert.Equal(69_883, result)
+        | Error error -> Assert.Fail(error)
+
+    [<Fact>]
+    member this.``Day 1 Star 2``() =
+        match Day1.calculateElfSupplies logger Day1.StarSwitch.StarTwo data with
+        | Ok result -> Assert.Equal(207_576, result)
+        | Error error -> Assert.Fail(error)
