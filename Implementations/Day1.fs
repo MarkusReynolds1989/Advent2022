cee843b6fa1e590ce3e0fb802ed7d1f1032bb1e6
Markus Reynolds <markusreynolds1989@gmail.com>
2025-11-28 18:20:37 -0600

    arch: introduce strategy pattern.

--- Implementations/Day1.fs
+++ Implementations/Day1.fs
@@ -14,6 +14,25 @@ type StarSwitch =
     | StarOne
     | StarTwo
 
+type CalculationStrategy = ILogger -> int list -> Result<int, string>
+
+let maxStrategy (logger: ILogger) (elfSupplies: int list) : Result<int, string> =
+    logger.LogInformation("Getting the maximum elf supplies")
+    let final = elfSupplies |> Seq.max
+    logger.LogInformation("Maximum is: {Final}", final)
+    Ok final
+
+let topThreeSumStrategy (logger: ILogger) (elfSupplies: int list) : Result<int, string> =
+    logger.LogInformation("Getting the top three elf supplies")
+    let final = elfSupplies |> Seq.sortDescending |> Seq.take 3 |> Seq.sum
+    logger.LogInformation("Sum of top three is: {Final}", final)
+    Ok final
+
+let getStrategy (starSwitch: StarSwitch) : CalculationStrategy =
+    match starSwitch with
+    | StarOne -> maxStrategy
+    | StarTwo -> topThreeSumStrategy
+
 let parseLine (logger: ILogger) (line: string) : Result<ListLine, string> =
     let mutable output = ref 0
 
@@ -41,32 +60,27 @@ let parseElfSuppliesInput (logger: ILogger) (lines: string seq) : Result<ListLin
                 | Ok parsedValue -> Ok(parsedValue :: accumulatedList)
                 | Error err -> Error err)
         (Ok [])
+    |> Result.map List.rev
 
-let createElfSupplies (logger: ILogger) (lines: ListLine seq) : int seq =
+let createElfSupplies (logger: ILogger) (lines: ListLine list) : int list =
+    let rec loop (returnList: int list) (acc: int) (lines: ListLine list) =
+        match lines with
+        | head :: tail ->
+            match head with
+            | End -> loop (acc :: returnList) 0 tail
+            | Line number -> loop returnList (acc + number) tail
+        | [] ->
+            logger.LogInformation("Completed creating elf supplies")
+            returnList
+
     logger.LogInformation("Creating elf supplies")
-    let mutable accumulator = 0
-
-    seq {
-        for line in lines do
-            match line with
-            | End ->
-                yield accumulator
-                accumulator <- 0
-            | Line number -> accumulator <- accumulator + number
-    }
+    loop [] 0 lines
 
 let calculateElfSupplies (logger: ILogger) (starSwitch: StarSwitch) (lines: string seq) : Result<int, string> =
+    let strategy = getStrategy starSwitch
+
     parseElfSuppliesInput logger lines
-    |> Result.bind (fun parsedSupplies -> Ok(createElfSupplies logger parsedSupplies))
-    |> Result.bind (fun elfSupplies ->
-        match starSwitch with
-        | StarOne ->
-            logger.LogInformation("Getting the maximum elf supplies")
-            let final = elfSupplies |> Seq.max
-            logger.LogInformation("Maximum is: {Final}", final)
-            Ok final
-        | StarTwo ->
-            logger.LogInformation("Getting the top three elf supplies")
-            let final = elfSupplies |> Seq.sortDescending |> Seq.take 3 |> Seq.sum
-            logger.LogInformation("Sum of top three is: {Final}", final)
-            Ok final)
+    |> Result.bind (fun parsedSupplies ->
+        let totalsList = createElfSupplies logger parsedSupplies
+        Ok totalsList)
+    |> Result.bind (strategy logger)
